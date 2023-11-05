b4cd557984a19d1e0825b6e945a788a7d740355c
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2023-11-05 18:19:01 -0500

    added more code and finished script file

--- script.js
+++ script.js
@@ -2,19 +2,50 @@ var ties = 0;
 var wins = 0;
 var losses = 0;
 
-var choice = ["Rock", "Paper", "Scissors"];
+var choices = ["Rock", "Paper", "Scissors"];
 
-var playGame = function() {
-    var userChoice = window.prompt("Rock, Paper, or Scissors");
+function playGame() {
+   var userChoice = window.prompt("Rock, Paper, or Scissors");
 
-    if(!userChoice) {
+    if (!userChoice) {
         return;
     }
 
-    var response = Math.floor(Math.random * choice.length);
-    computerChoice = choice[i];
+    userChoice = userChoice.toUpperCase();
 
+    var response = Math.floor(Math.random() * choices.length);
+    var computerChoice = choices[response].toUpperCase();
+
+    if (computerChoice === userChoice) {
+        ties++;
+        window.alert("Tie!");
+    } else if (userChoice === "ROCK" && computerChoice === "SCISSORS") {
+        wins++;
+        window.alert("You Won!");
+    } else if (userChoice === "PAPER" && computerChoice === "ROCK") {
+        wins++;
+        window.alert("You Won!");
+    } else if (userChoice === "SCISSORS" && computerChoice === "PAPER") {
+        wins++;
+        window.alert("You Won!");
+    } else {
+        losses++;
+        window.alert("You lost!");
+    }
     
+
+    window.alert("Wins: " + wins + " Ties: " + ties + " Losses: " + losses);
+
+   var playAgain = window.confirm("Would you like to play again?");
+
+    if (playAgain) {
+        playGame();
+    }
+
 }
+
+playGame();
+
+
 
 
